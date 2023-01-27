cb6f22b305b178cc7496880d3e06e2bb6f6204fd
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2023-01-27 12:39:18 +0200

    git init

---
+++ routes/api/employees.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router =  express.Router();
+
+const data = {};
+
+data.employees = require('../../data/employees.json');
+ 
+router.route('/')
+.get((req,res)=>{
+    res.json(data.employees);
+    console.log(req.method);
+})
+.post((req,res)=>{
+    res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname,
+
+    }
+    )
+})
+.put((req,res)=>{
+    res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname,
+
+    }
+    )  
+}) 
+.delete((req,res)=>{
+    res.json({
+        "id": req.body.id
+    })
+})
+
+router.route('/:id')
+.get((req,res)=>[
+    res.json({
+        "id": req.params.id
+    })
+])
+.put((req,res)=>{
+    res.json(
+        {
+            "id": req.params.id
+        }
+    )
+})
+
+module.exports = router;
+
